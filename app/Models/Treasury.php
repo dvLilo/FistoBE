542920889a25616aee9026319f9616546dbcc763
darkanorep <jeromeperona@yahoo.com>
2023-11-16 09:32:45 +0800

    add dept,loc,comp, etc. in cheque's account title

--- app/Models/Treasury.php
+++ app/Models/Treasury.php
@@ -22,7 +22,23 @@ class Treasury extends Model
 
     public function account_title(){
         return $this->hasMany(VoucherAccountTitle::class,'treasury_id','id')->select('id','treasury_id','entry',
-        'account_title_id','account_title_name','amount','remarks','transaction_type');
+        'account_title_id','account_title_name','amount','remarks','transaction_type',
+            'company_id',
+            'company_code',
+            'company_name',
+            'department_id',
+            'department_code',
+            'department_name',
+            'location_id',
+            'location_code',
+            'location_name',
+            'business_unit_id',
+            'business_unit_code',
+            'business_unit_name',
+            'sub_unit_id',
+            'sub_unit_code',
+            'sub_unit_name',
+        );
     }
 
     public function cheques(){
